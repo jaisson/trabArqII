dc0ee5ae8a2f7b36797d4c7d7eb850c1f6f6d15f
jaisson <jaissonduarte@yahoo.com.br>
2023-08-14 13:44:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,152 @@
+var perguntas = [
+  {
+   "Qual desses nao eh um Sistema Operacional:": {
+     "gabarito":"4",
+     "lido":"0",
+     "resposta1":"Windows",
+     "resposta2":"Linux",
+     "resposta3":"Unix",
+     "resposta4":"Java"
+   }
+  },
+  {
+   "Eh um dos principais componentes do sistema operacional:": {
+     "gabarito":"3",
+     "lido":"0",
+     "resposta1":"Placa Mae",
+     "resposta2":"Hardware",
+     "resposta3":"Gerenciamento de Memoria",
+     "resposta4":"Escalonamento de Processos"
+   }
+  },
+  {
+   "O que implementa o Gerenciador de Arquivo:": {
+     "gabarito":"1",
+     "lido":"0",
+     "resposta1":"Gerenciamento de espacos livres",
+     "resposta2":"Criacao e delecao de processos",
+     "resposta3":"Alocacao e liberacao de espaco de memoria",
+     "resposta4":"Gerenciar a execucao dos programas"
+   }
+  },
+  {
+   "Em que linguagem foi reescrito o UNIX:": {
+     "gabarito":"4",
+     "lido":"0",
+     "resposta1":"Assembly",
+     "resposta2":"C#",
+     "resposta3":"Basic",
+     "resposta4":"C"
+   }
+  },
+  {
+   "Segundo analises de seguranca, qual o melhor sistema operacional?": {
+     "gabarito":"4",
+     "lido":"0",
+     "resposta1":"Windows",
+     "resposta2":"IOs Mac",
+     "resposta3":"Symbian",
+     "resposta4":"Linux"
+   }
+  },
+  {
+   "Qual o sistema operacional com maior numero de versoes?": {
+     "gabarito":"3",
+     "lido":"0",
+     "resposta1":"Android",
+     "resposta2":"Windows",
+     "resposta3":"Linux",
+     "resposta4":"IOs Mac"
+   }
+  },
+  {
+   "Eh uma das caracteristicas de um Sistema Operacional Multiprogramado(Multitarefa)": {
+     "gabarito":"3",
+     "lido":"0",
+     "resposta1":"Eh multi ou mono programado",
+     "resposta2":"O processador fica ocioso",
+     "resposta3":"Executado por um ou varios processadores",
+     "resposta4":"Possui varios processadores"
+   }
+  },
+  {
+   "Caracteristica de um Sistema Operacional Monoprogramado(Monotarefa)": {
+     "gabarito":"1",
+     "lido":"0",
+     "resposta1":"Monousuario",
+     "resposta2":"Ocorre paralelismo fisico ou real",
+     "resposta3":"Time-Sharing",
+     "resposta4":"Possui varios processadores"
+   }
+  },
+  {
+   "Sistema operacional fracamente acoplados eh:": {
+     "gabarito":"1",
+     "lido":"0",
+     "resposta1":"Tem a sua propria memoria e sistema operacional",
+     "resposta2":"Os processadores compartilham uma unica memoria",
+     "resposta3":"os programas enfileirados e aguardam a execucao",
+     "resposta4":"Sao sistemas multiterminais"
+   }
+  },
+  {
+   "Plug-and-Play, foi implantado primeiro em:": {
+     "gabarito":"3",
+     "lido":"0",
+     "resposta1":"UNIX",
+     "resposta2":"Windows NT",
+     "resposta3":"Windows 95",
+     "resposta4":"MS-DOS"
+   }
+  },
+  {
+   "Sistema operacional mais caro:": {
+     "gabarito":"4",
+     "lido":"0",
+     "resposta1":"Linux",
+     "resposta2":"Windows",
+     "resposta3":"Firefox",
+     "resposta4":"IOS Mac"
+   }
+  },
+  {
+   "Bufferizacao esta presente em qual gerenciador:": {
+     "gabarito":"2",
+     "lido":"0",
+     "resposta1":"Gerenciador de Processos",
+     "resposta2":"Grenciador de Arquivos",
+     "resposta3":"Gerenciador de Memoria",
+     "resposta4":"Gerenciador de I/O"
+   }
+  },
+  {
+   "Sistema operacional mais utilizado no mundo:": {
+     "gabarito":"3",
+     "lido":"0",
+     "resposta1":"IOs Mac",
+     "resposta2":"Linux",
+     "resposta3":"Windows",
+     "resposta4":"Android"
+   }
+  },
+  {
+   "O que eh tightly coupled": {
+     "gabarito":"4",
+     "lido":"0",
+     "resposta1":"Cada processador possui sua propria memoria",
+     "resposta2":"Semelhantes aos sistemas time-sharing",
+     "resposta3":"Sistemas de simples implementacao",
+     "resposta4":"Processadores compartilham uma unica memoria"
+   }
+  },
+  {
+   "Sistema operacional com maior numero de virus:": {
+     "gabarito":"4",
+     "lido":"0",
+     "resposta1":"Linux",
+     "resposta2":"Polaris",
+     "resposta3":"IOs Mac",
+     "resposta4":"Windows"
+   }
+  }
+]
